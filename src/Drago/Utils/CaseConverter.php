5c81b57687341323f1a1df7950dc803f4df35d16
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-21 07:41:19 +0200

    Update

--- src/Drago/Utils/CaseConverter.php
+++ src/Drago/Utils/CaseConverter.php
@@ -1,37 +1,26 @@
 <?php
 
-/**
- * Drago Extension
- * Package built on Nette Framework
- */
 declare(strict_types=1);
 
 namespace Drago\Utils;
 
 
-/**
- * This utility class provides methods for converting strings between different
- * case formats, such as converting from CamelCase to snake_case.
- */
+/** Provides methods for converting strings between different case formats. */
 class CaseConverter
 {
-	/**
-	 * Converts a string from CamelCase to snake_case.
-	 *
-	 * Example:
-	 *   'CamelCaseString' => 'camel_case_string'
-	 *
-	 * @param string $input The CamelCase string to be converted.
-	 * @return string The converted snake_case string.
-	 */
+	/** Converts a string from CamelCase to snake_case. */
 	public static function snakeCase(string $input): string
 	{
-		// If the string doesn't contain any uppercase letters, return it as is.
 		if (!preg_match('/[A-Z]/', $input)) {
 			return $input;
 		}
 
-		// Use regex to convert CamelCase to snake_case.
-		return strtolower(preg_replace_callback('/([a-z])([A-Z])/', fn(array $matches) => $matches[1] . '_' . strtolower($matches[2]), $input));
+		return strtolower(
+			(string) preg_replace_callback(
+				'/([a-z])([A-Z])/',
+				fn(array $matches): string => $matches[1] . '_' . strtolower((string) $matches[2]),
+				$input,
+			),
+		);
 	}
 }
--- src/Drago/Utils/ExtraArrayHash.php
+++ src/Drago/Utils/ExtraArrayHash.php
@@ -1,9 +1,5 @@
 <?php
 
-/**
- * Drago Extension
- * Package built on Nette Framework
- */
 declare(strict_types=1);
 
 namespace Drago\Utils;
@@ -11,35 +7,22 @@ namespace Drago\Utils;
 use Nette\Utils\ArrayHash;
 
 
-/**
- * This class extends Nette's ArrayHash and adds additional functionality to
- * convert the stored data to arrays, with options to convert keys to uppercase.
- */
+/** Extends ArrayHash with convenience methods for array conversion. */
 class ExtraArrayHash extends ArrayHash
 {
-	/**
-	 * Converts the items in the object to a simple array.
-	 *
-	 * This method returns the internal data of the ExtraArrayHash object as an array.
-	 *
-	 * @return array The converted array.
-	 */
+	/** @return array<string, mixed> */
 	public function toArray(): array
 	{
-		return (array) $this;
+		/** @var array<string, mixed> $data */
+		$data = (array) $this;
+		return $data;
 	}
 
 
-	/**
-	 * Converts the items in the object to an array with all keys converted to uppercase.
-	 *
-	 * This method returns the internal data of the ExtraArrayHash object as an array,
-	 * but changes all the keys to uppercase.
-	 *
-	 * @return array The converted array with uppercase keys.
-	 */
+	/** @return array<string, mixed> */
 	public function toArrayUpper(): array
 	{
+		/** @var array<string, mixed> $data */
 		$data = (array) $this;
 		return array_change_key_case($data, CASE_UPPER);
 	}
